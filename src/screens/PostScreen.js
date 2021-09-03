459326cbc508b12a8f7385172ab231eb28d284e2
Eugene <evgeniq.makcimov@yandex.ru>
2021-09-04 00:07:45 +0300

    add select post

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-export const PostScreen = ({}) => {
+export const PostScreen = ({ navigation }) => {
+  const postId = navigation.getParam('postId')
   return (
     <View style={styles.center}>
-      <Text>PostScreen</Text>
+      <Text>{postId}</Text>
     </View>
   )
 }
 
-PostScreen.navigationOptions = {
-  headerTitle: 'Пост 12',
-  headerStyle: {
-    backgroundColor: '#5F2580',
-  },
-  headerTintColor: '#fff',
+PostScreen.navigationOptions = ({ navigation }) => {
+  const date = navigation.getParam('date')
+  return {
+    headerTitle: 'Пост от ' + new Date(date).toLocaleDateString(),
+  }
 }
 
 const styles = StyleSheet.create({
