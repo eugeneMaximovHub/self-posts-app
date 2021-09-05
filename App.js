fdf8e1b16ab05377def44b33fe73ba2f4ef50068
Eugene <evgeniq.makcimov@yandex.ru>
2021-09-05 20:12:38 +0300

    first add Redux

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import AppLoading from 'expo-app-loading';
+import AppLoading from 'expo-app-loading'
+import { Provider } from 'react-redux'
 import { AppNavigation } from './src/navigation/AppNavigation'
-import  {bootstrap}  from './src/bootstrap'
+import { bootstrap } from './src/bootstrap'
+import store from './src/store/index'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
@@ -12,11 +13,14 @@ export default function App() {
       <AppLoading
         startAsync={bootstrap}
         onFinish={() => setIsReady(true)}
-        onError={err => console.log(err)}
+        onError={(err) => console.log(err)}
       />
     )
   }
 
-  return <AppNavigation />
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  )
 }
-
