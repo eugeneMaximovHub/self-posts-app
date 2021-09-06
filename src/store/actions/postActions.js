2c839fea473e3e64baa777ab658022ca7f12f3e1
Eugene <evgeniq.makcimov@yandex.ru>
2021-09-06 19:38:57 +0300

    updating and deleting

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -13,18 +13,22 @@ export const loadPosts = () => {
   }
 }
 
-export const toogleBooked = (id) => {
-  return {
+export const toogleBooked = (post) => async (dispatch) => {
+  await DB.updatePost(post)
+
+  dispatch({
     type: TOGGLE_BOOKED,
-    payload: id,
-  }
+    payload: post.id,
+  })
 }
 
-export const removePost = (id) => {
-  return {
+export const removePost = (id) => async (dispatch) => {
+  await DB.removePost(id)
+
+  dispatch({
     type: REMOVE_POST,
     payload: id,
-  }
+  })
 }
 
 export const addPost = (post) => async (dispatch) => {
