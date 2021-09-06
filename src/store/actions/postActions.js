76732466b857c362d08783b73c96b4f105aba542
Eugene <evgeniq.makcimov@yandex.ru>
2021-09-06 19:25:48 +0300

    creating record

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,3 +1,4 @@
+import * as FileSystem from 'expo-file-system'
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from '../types'
 import { DB } from '../../db'
 
@@ -26,10 +27,27 @@ export const removePost = (id) => {
   }
 }
 
-export const addPost = (post) => {
-  post.id = Date.now().toString()
-  return {
-    type: ADD_POST,
-    payload: post,
+export const addPost = (post) => async (dispatch) => {
+  const fileName = post.img.split('/').pop()
+  const newPath = FileSystem.documentDirectory + fileName
+
+  try {
+    await FileSystem.moveAsync({
+      to: newPath,
+      from: post.img,
+    })
+  } catch (e) {
+    console.log('Error:', e)
   }
+
+  const payload = { ...post, img: newPath }
+
+  const id = await DB.createPost(payload)
+
+  payload.id = id
+
+  dispatch({
+    type: ADD_POST,
+    payload,
+  })
 }
